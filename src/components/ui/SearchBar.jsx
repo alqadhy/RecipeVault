23815552f629b90d351f76825fd2ef8ded432456
alqadhy <abdelrahmankhalidalqadhy@gmail.com>
2026-06-03 21:01:29 +0300

    Next.js SearchBar built error fixed

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,5 +1,9 @@
 "use client";
 
+// Components
+import { Suspense } from "react";
+import Loader from "../layout/Loader";
+
 // Icons
 import { Search } from "lucide-react";
 
@@ -36,24 +40,26 @@ function SearchBar({ placeholder, className = "" }) {
   }
 
   return (
-    <form
-      className={`search-bar relative ${className}`}
-      onSubmit={handleSubmit}
-    >
-      <Search
-        size={20}
-        className="text-muted-foreground absolute left-4 top-1/2 -translate-y-1/2 size-5"
-      />
-      <input
-        type="text"
-        name="s"
-        placeholder={placeholder}
-        title="Search recipes"
-        className="block w-full h-14 pl-12 pr-4 border-2 border-sidebar-border rounded-lg transition-colors focus:border-theme"
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
-      />
-    </form>
+    <Suspense fallback={<Loader />}>
+      <form
+        className={`search-bar relative ${className}`}
+        onSubmit={handleSubmit}
+      >
+        <Search
+          size={20}
+          className="text-muted-foreground absolute left-4 top-1/2 -translate-y-1/2 size-5"
+        />
+        <input
+          type="text"
+          name="s"
+          placeholder={placeholder}
+          title="Search recipes"
+          className="block w-full h-14 pl-12 pr-4 border-2 border-sidebar-border rounded-lg transition-colors focus:border-theme"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </form>
+    </Suspense>
   );
 }
 
